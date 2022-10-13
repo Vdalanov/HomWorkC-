ac1209f968819c4de995ea09bf8c0b6cf873f92b
Vladislav <vdalanov@yandex.ru>
2022-10-13 23:55:47 +0300

    work 3

--- Seminar02/Program.cs
+++ Seminar02/Program.cs
@@ -1,14 +1,94 @@
-﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+﻿// //Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-int FindQuart (double x, double y)
+// int FindQuart (double x, double y)
+// {
+//     if(x>0 && y>0) return 1;
+//     if(x<0 && y>0) return 2;
+//     if(x<0 && y<0) return 3;
+//     if(x>0 && y<0) return 4;
+//     return 0;
+// }
+// Console.Write("Input x-coordinate: ");
+// double x = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Input y-coordinate: ");
+// double y = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"The poin A({x},{y}) is on the {FindQuart(x,y)} quart");
+
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+// void FindQuart (int a)
+// {
+//     if(a==1)  Console.Write("У вас должен быть пложительный X и Y");
+//     else if(a==2)  Console.Write("У вас должен быть отрицательный X и пложительный Y");
+//     else if(a==3)  Console.Write("У вас должен быть отрицательный X и отрицательный Y");
+//     else if(a==4)  Console.Write("У вас должен быть пложительный X и отрицательный Y" );
+//  else
+//  {
+//     Console.WriteLine("Ваше чилсо не соотвествует номеру четверти");
+//  }
+// }
+// Console.WriteLine("Введите номер четверти: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// FindQuart(n);
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// double coordinate (double xa, double ya ,double xb, double yb)
+// {
+//     return(Math.Sqrt(Math.Pow((xa-xb),2)+ Math.Pow((ya-yb),2)));
+// }
+// Console.WriteLine("Введите координаты: ");
+// double xa = Convert.ToDouble(Console.ReadLine());
+// double ya = Convert.ToDouble(Console.ReadLine());
+// double xb = Convert.ToDouble(Console.ReadLine());
+// double yb = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine ($"Растоняние между точками ровно {coordinate(xa,ya,xb,yb)}");
+
+
+
+
+// // Напишите программу, которая принимает на вход число (N) и выводит квадраты чисел от 1 до N.
+// void writeq(int n)
+// {
+//     int current=1;
+//     int sum= 0;
+//     while(n>=current)
+//     {
+//         sum=current*current;
+//         Console.Write($"{sum} " );
+//         current++;
+//     }
+// }
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// writeq(n);
+
+// Задача 19. Напишите программу, которая принимает на вход  число и проверяет, является ли оно палиндромом.
+bool Palindrome(int num)
 {
-    if(x>0 && y>0) return 1;
-    if(x<0 && 
-    )
+    bool res = true;
+    while (num > 9)
+    {
+        int lastDigit = num % 10;
+        int firstDigit = num;
+        int countOfDigits = 0;
+        while (firstDigit >= 10)
+        {
+            countOfDigits++;
+            firstDigit /= 10;
+        }
+        if (lastDigit != firstDigit)
+        {
+            res = false;
+            break;
+        }
+        num = num / 10 % (Convert.ToInt32(Math.Pow(10, countOfDigits - 1)));
+    }
+    return res;
 }
+Console.Write("Введите число, которое будем проверять на палиндром: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-
-void 
+Console.Write(Palindrome(number));
---
+++ Work003/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// void palindrom (int n)
+// {                      
+//         if (n < 10000 ^ n > 99999)
+//     {
+//         Console.WriteLine($" введено не пятизначное число.");
+//     }
+//     else
+//     {            
+//     int a = n;                   
+//         if (a/10000 == a%10) a = a/10%1000;
+//         if (a/100 == a%10)Console.WriteLine($"Your number {n} it is a palindrome");
+//         else 
+//         Console.WriteLine ($"Your number {n}  is not a palindrom" ); 
+//     }
+// }
+
+// Console.WriteLine("Input a number: ");
+// int n=Convert.ToInt32(Console.ReadLine());
+// palindrom(n);
+  
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// double Distance(double ax, double ay, double az, double bx, double by, double bz)
+
+
+// {
+//     return Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+// }
+
+// Console.WriteLine("Введите координаты точки A : ax,ay,az: ");
+// double ax = Convert.ToDouble(Console.ReadLine());
+// double ay = Convert.ToDouble(Console.ReadLine());
+// double az = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Введите координаты  точки B: bx,by,bz ");
+// double bx = Convert.ToDouble(Console.ReadLine());
+// double by = Convert.ToDouble(Console.ReadLine());
+// double bz = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"Расстояние между точкой A и b равно равно {Distance(ax, ay, az, bx, by, bz)}");
+
+
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void Kub(int n)
+{
+    int count = 1;
+    while (count <= n)
+    {
+        Console.Write($"{Math.Pow(count, 3)}");
+        if (count < n) Console.Write(", "); 
+        count++;
+    }
+}
+Console.WriteLine("Введите число  ");
+int x = Convert.ToInt32(Console.ReadLine());
+Kub(x);
+
